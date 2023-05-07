8d2cee3f3ae8f8603a2146f225c8b5dbb4501ee8
USERNAME <EMAIL>
2023-05-07 12:30:00 -0400

    Added db files and content

---
+++ back-end/db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO snack (name, description, price, calories) VALUES   ('Chips', 'Crispy and savory potato chips', 1.99, 150),   
+('Chocolate Bar', 'Rich and creamy chocolate bar', 2.49, 200),   ('Popcorn', 'Classic buttery popcorn', 1.49, 100),   
+('Granola Bar', 'Healthy and delicious granola bar', 1.79, 120),   ('Pretzels', 'Crunchy and salty pretzel sticks', 1.29, 110);
+
