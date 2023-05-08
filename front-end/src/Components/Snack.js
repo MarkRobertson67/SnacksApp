8822872078755edfb0ce48c3b23c3cb14e02dc07
Mark Robertson <markrobertson67@gmail.com>
2023-05-07 20:11:14 -0400

    Added Snacks Component

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -4,6 +4,35 @@ import { useState, useEffect } from "react";
 const API = process.env.REACT_APP_API_URL;
 
 function Snacks() {
+    const [snacks, setSnacks] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`${API}/snacks`)
+        .then((response) => setSnacks(response.data))
+        .catch((error) => console.warn("catch", error))
+      }, []);
+
+      return (
+        <div className="Snacks">
+          <section>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Take me there</th>
+                  <th>See this snack</th>
+                </tr>
+              </thead>
+              <tbody>
+                {snacks.map((snack) => {
+                  return <Snack key={snack.id} snack={snack} />;
+                })}
+              </tbody>
+            </table>
+          </section>
+        </div>
+      );
 
 }
 
