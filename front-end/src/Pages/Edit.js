c6211badaba3c74a229ef962e2dd54f2a48bc011
Wilsantos1975 <Faustosantos@pursuit.org>
2023-05-08 21:30:28 -0400

    working on the front end

--- front-end/src/Pages/Edit.js
+++ front-end/src/Pages/Edit.js
@@ -1,10 +1,10 @@
-import SnacksEditForm from "../Components/SnacksEditForm";
+import SnackEditForm from "../Components/SnackEditForm";
 
 function Edit() {
   return (
     <div className="New Edit">
       <h2>Edit Snack</h2>
-      <SnacksEditForm />
+      <SnackEditForm />
     </div>
   );
 }
