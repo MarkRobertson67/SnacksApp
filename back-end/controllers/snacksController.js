4f3bcc2c40a31a68ddf26ba1f6feb851c58b97bb
Wilsantos1975 <Faustosantos@pursuit.org>
2023-05-08 20:02:08 -0400

    all snack routes created

--- back-end/controllers/snacksController.js
+++ back-end/controllers/snacksController.js
@@ -1,20 +1,71 @@
 const express = require("express");
 const snacks = express.Router();
 
-const { getAllSnacks } = require("../queries/snacksQueries");
-
-
+const {
+  getAllSnacks,
+  getASnack,
+  addASnack,
+  deleteASnack,
+  updateASnack,
+} = require("../queries/snacksQueries");
 
 // INDEX route
 snacks.get("/", async (req, res) => {
-    const allSnacks = await getAllSnacks();
+  const allSnacks = await getAllSnacks();
 
-    if (allSnacks) {
-        res.status(200).json(allSnacks);
-      } else {
-        res.status(500).json({ error: "server error" });
-      }
+  if (allSnacks) {
+    res.status(200).json(allSnacks);
+  } else {
+    res.status(404).json({ error: "server error" });
+  }
 });
 
+// SHOW route
+snacks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const snack = await getASnack(id);
+console.log("show route sucessfully reached")
+  if (snack) {
+    console.log(snack);
+    res.status(200).json(snack);
+    console.log(snack)
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE route
+snacks.post("/", async (req, res) => {
+  const newSnack = await addASnack(req.body);
+  try {
+    const addedSnack = await addASnack(newSnack);
+    res.status(200).json(addedSnack);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// DELETE route
+snacks.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedSnack = await deleteASnack(id);
+    res.status(200).json(deletedSnack);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// UPDATE route
+snacks.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const snack = req.body;
+  try {
+    const updatedSnack = await updateASnack(id, snack);
+    res.status(200).json(updatedSnack);
+  } catch (error) {
+    res.status(404).json({ error: "not found" });
+  }
+});
 
 module.exports = snacks;
