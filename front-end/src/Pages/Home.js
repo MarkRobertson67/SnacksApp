3e72dd109e9066cbbdd116512fa65cb218764123
Mark Robertson <markrobertson67@gmail.com>
2023-05-10 20:44:42 -0400

    readMe done except to add some photos etc

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,10 +1,14 @@
 
 function Home() {
     return (
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <div>
         <h2>Welcome</h2>
+      </div>
+      <div>
         <h3>To the PigOut Snacks App</h3>
       </div>
+    </div>
     );
   }
   
